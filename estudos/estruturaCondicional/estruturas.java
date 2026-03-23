8dcd30041650d956f2e642e21fa5de29ad7d236a
osmarwilliam <osmarwilliam4@gmail.com>
2026-03-22 21:50:41 -0300

    adicionando estudo sobre estrutura condicional

---
+++ estudos/estruturaCondicional/estruturas.java
@@ -0,0 +1,54 @@
+package estudos.estruturaCondicional;
+
+public class estruturas {
+    public static void main(String[] args) {
+        /*
+         * operadores:
+         * &&
+         * ||
+         * !
+         *
+         */
+
+        /*
+         * Condicional:
+         *
+         * if (condicao1) {
+         * // Executa se condicao1 for verdadeira
+         * } else if (condicao2) {
+         * if () {
+         * } else {
+         * }
+         * } else {
+         * // Executa se todas as anteriores forem falsas
+         * }
+         */
+
+        /*
+         * switch case:
+         *
+         * switch (){
+         * case 1:
+         * // info
+         * break;
+         * case 2:
+         * // info
+         * break;
+         * case 3:
+         * //info
+         * break;
+         * default:
+         * // valor padrão
+         * break;
+         * }
+         */
+
+        /*
+         * operador ternario:
+         * (condicao)? valor_verdadeiro : valor_falso;
+         *
+         *
+         */
+
+    }
+}
---
+++ estudos/estruturaSequencial/funcoesMatematicas.java
@@ -0,0 +1,10 @@
+
+public class funcoesMatematicas {
+    public static void main(String[] args) {
+        // A = Math.pow(x, y);
+        // A = Math.sqrt(x);
+        // A = Math.abs(x);
+
+    }
+
+}
